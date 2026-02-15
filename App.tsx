e85fa575fd1f8c439230b8774e9efa22f7299ac1
SekharYangala <reddysekharabc@gmail.com>
2026-02-15 13:58:06 +0530

    Ready and everything installed

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AuthProvider } from './src/context/AuthContext';
-import AppNavigator from '../InstaLoginApp/src/navigation/AppNavigator'
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
